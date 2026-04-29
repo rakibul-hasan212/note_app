bbb499454dadc6d78f85e062b79f90f4993a0d8d
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-04-29 20:23:29 +0600

    Seperate the splashpage logic on splash_services file

---
+++ lib/pages/splash/splash_services.dart
@@ -0,0 +1,25 @@
+
+import 'package:firebase_project/controller/auth/auth_controller.dart';
+import 'package:get/get.dart';
+
+import '../home/home_page.dart';
+import '../login/login_page.dart';
+
+void startApp() async{
+  //initialization of AuthController
+  final authController = Get.put(AuthController());
+
+  //Splash screen er delay
+  await Future.delayed(Duration(seconds: 2));
+
+  //current user logged in or logged out tar value get
+  final user = authController.firebaseUser.value;
+
+  if(user != null){
+    // that means user login already so go to HomePage()
+    Get.offAll(()=> HomePage());//push.Replacement er kaj kore Get.offAll()
+  }else{
+    //that means user have to login first, so go to LoginPage()
+    Get.offAll(()=> LoginPage());
+  }
+}
