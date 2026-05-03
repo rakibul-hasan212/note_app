40c5e7e65516cbb4ff9fc275635a992080c7d768
rakibul-hasan212 <egnrakibul212@gmail.com>
2026-05-03 14:38:54 +0600

    modify the UpdateNotePage body

--- lib/pages/update/update_note_page.dart
+++ lib/pages/update/update_note_page.dart
@@ -1,76 +1,42 @@
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_project/pages/update/controller/update_note_controller.dart';
+
+import 'package:firebase_project/controller/notes/notes_controller.dart';
+import 'package:firebase_project/model/note_model.dart';
+import 'package:firebase_project/widgets/Note%20widget/note_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../core/colors/app_colors.dart';
 
-class UpdateNotePage extends StatefulWidget{
-  final Map<String, dynamic> user;
-  UpdateNotePage({super.key, required this.user});
+class UpdateNotePage extends StatelessWidget{
+  final NoteModel note;
+  UpdateNotePage({super.key, required this.note});
+  late final title = TextEditingController(text: note.title);
+  late final subTitle = TextEditingController(text: note.subTitle);
+  final NoteController noteCntrl = Get.find();
 
-  @override
-  State<UpdateNotePage> createState() => _UpdateNotePageState();
-}
-
-class _UpdateNotePageState extends State<UpdateNotePage> {
-  final updateController = Get.put(UpdateNoteController());
-
-  @override
-  void initState() {
-    updateController.title.value.text = widget.user['title'];
-    updateController.subTitle.value.text = widget.user['subTitle'];
-    super.initState();
-  }
-  @override
-  void dispose() {
-    updateController;
-    super.dispose();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundPrimary,
       appBar: AppBar(
         backgroundColor: AppColors.backgroundDark,
-        title: Text("Update Note",style: TextStyle(color: AppColors.textPrimary,fontSize: 28,fontWeight: FontWeight.w800)),
+        title: Text("Note Update",style: TextStyle(color: AppColors.textPrimary,fontSize: 28,fontWeight: FontWeight.w800)),
         centerTitle: true,
         toolbarHeight: 60,
       ),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: updateController.title.value,
-              decoration: InputDecoration(
-                  hintText: "Enter the Title",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16)
-                  )
-              ),
-            ),
-            SizedBox(height: 10,),
-            TextFormField(
-              controller: updateController.subTitle.value,
-              decoration: InputDecoration(
-                  hintText: "Description...",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16)
-                  ),
-                  hintMaxLines: 4
-              ),
-            ),
-            SizedBox(height: 20,),
-            ElevatedButton(onPressed: () async{
-              await updateController.updateNote(widget.user['id'], updateController.title.value.text, updateController.subTitle.value.text);
-              Navigator.pop(context);
-            }, child: Text("Update"))
-          ],
-        ),
-      ),
+      body: NoteWidgets(
+          titleCntlr: title,
+          subTitleCntlr: subTitle,
+          buttonText: "Update Note",
+          onSubmit: () async {
+            NoteModel updateNote = NoteModel(
+                id: note.id,
+                title: title.text,
+                subTitle: subTitle.text);
+            await noteCntrl.updateNote(updateNote);
+            Get.back();
+          })
     );
   }
 }
